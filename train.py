886a5f727ced7f191c286892cee2f269d19db8d0
Aziz Amari <azizamari2003@gmail.com>
2023-12-03 00:35:39 +0100

    feat: support pdf files too

--- train.py
+++ train.py
@@ -1,16 +1,45 @@
 import argparse
+import os
+import PyPDF2
 from transformers import GPT2Tokenizer, GPT2LMHeadModel, TextDataset, DataCollatorForLanguageModeling
 from transformers import Trainer, TrainingArguments
 
+def extract_text_from_pdf(pdf_path):
+    with open(pdf_path, 'rb') as file:
+        reader = PyPDF2.PdfFileReader(file)
+        text = ''
+        for page in range(reader.numPages):
+            text += reader.getPage(page).extractText()
+    return text
+
+def prepare_data(data_dir):
+    all_text = ''
+    for filename in os.listdir(data_dir):
+        file_path = os.path.join(data_dir, filename)
+        if filename.endswith('.txt'):
+            with open(file_path, 'r', encoding='utf-8') as file:
+                all_text += file.read()
+        elif filename.endswith('.pdf'):
+            all_text += extract_text_from_pdf(file_path)
+    return all_text
+
 def train_model(data_dir, model_variant, output_dir, num_train_epochs, batch_size):
-    # Load tokenizer and model based on the specified variant
+    # Prepare data
+    data_text = prepare_data(data_dir)
+
+    # Save combined data to a temporary file (optional)
+    temp_data_file = os.path.join(data_dir, 'temp_combined_data.txt')
+    with open(temp_data_file, 'w', encoding='utf-8') as file:
+        file.write(data_text)
+
+    # Load tokenizer and model
     tokenizer = GPT2Tokenizer.from_pretrained(model_variant)
     model = GPT2LMHeadModel.from_pretrained(model_variant)
 
     # Load dataset
     train_dataset = TextDataset(
         tokenizer=tokenizer,
-        file_path=data_dir,
+        file_path=temp_data_file,
         block_size=128)
 
     data_collator = DataCollatorForLanguageModeling(
